8a4df8b3248604ab68b2214008318e3321d446ce
Abhishek Kumar Singh <119054351+12203279abhi@users.noreply.github.com>
2022-12-08 22:28:07 +0530

    Add files via upload

---
+++ Rock paper game.py
@@ -0,0 +1,10 @@
+s1=input()
+s2=input()
+if s1==s2 :
+    print("Tie")
+elif s1=="Rock" and s2=="Paper" :
+    print("Anjali Wins")
+elif s1=="Scissors" and s2=="Rock" :
+    print("Anjali Wins")
+else :
+    print("Abhinav Wins")
